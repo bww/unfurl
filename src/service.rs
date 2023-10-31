fdb13cc963d541b81de0a84b5ca23455c1025ad8
Brian W. Wolter <brianwolter@gmail.com>
2023-10-31 07:50:53 -0700

    ...

--- src/service.rs
+++ src/service.rs
@@ -22,7 +22,7 @@ struct GithubConfig {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct PullRequest {
+struct GithubIssue {
   number: usize,
   title: String,
 }
@@ -30,7 +30,9 @@ struct PullRequest {
 pub struct Github{
   client: blocking::Client,
   config: GithubConfig,
+
   pattern_pr: route::Pattern,
+  pattern_issue: route::Pattern,
 }
 
 impl Github {
@@ -43,18 +45,12 @@ impl Github {
       client: blocking::Client::new(),
       config: serde_yaml::from_value(conf.auth.clone())?,
       pattern_pr: route::Pattern::new("/{org}/{repo}/pull/{num}"),
+      pattern_issue: route::Pattern::new("/{org}/{repo}/issues/{num}"),
     })
   }
-}
 
-
-impl Service for Github {
-  fn unfurl(&self, conf: &config::Config, link: &url::Url) -> Result<String, error::Error> {
-    let pr = match self.pattern_pr.match_path(link.path()) {
-      Some(pr) => pr,
-      None     => return Ok(link.to_string()),
-    };
-    let num = match pr.get("num") {
+  fn unfurl_pr(&self, conf: &config::Config, link: &url::Url, mat: route::Match) -> Result<String, error::Error> {
+    let num = match mat.get("num") {
       Some(num) => num,
       None      => return Ok(link.to_string()),
     };
@@ -68,16 +64,48 @@ impl Service for Github {
         Err(err) => return Ok(format!("{} ({})", link, err)),
     };
 
-    let resp: PullRequest = match resp.json::<PullRequest>() {
+    let resp: GithubIssue = match resp.json::<GithubIssue>() {
       Ok(resp) => resp,
       Err(err) => return Ok(format!("{} ({})", link, err)),
     };
 
     Ok(format!("{} (#{})", resp.title, resp.number))
-    // let conf = match conf.get(DOMAIN_GITHUB) {
-    //   Some(conf) => Ok(format!("<<{}>> (Github! + {:?})", link, conf)),
-    //   None       => Err(error::Error::NotFound),
-    // }
+  }
+
+  fn unfurl_issue(&self, conf: &config::Config, link: &url::Url, mat: route::Match) -> Result<String, error::Error> {
+    let num = match mat.get("num") {
+      Some(num) => num,
+      None      => return Ok(link.to_string()),
+    };
+
+    let resp: blocking::Response = match self.client.get(&format!("https://api.github.com/repos/treno-io/product/issues/{}", num))
+      .header("Accept", "application/vnd.github+json")
+      .header("User-Agent", &format!("Unfurl/{}", VERSION))
+      .header("Authorization", &self.config.header)
+      .send() {
+        Ok(resp) => resp,
+        Err(err) => return Ok(format!("{} ({})", link, err)),
+    };
+
+    let resp: GithubIssue = match resp.json::<GithubIssue>() {
+      Ok(resp) => resp,
+      Err(err) => return Ok(format!("{} ({})", link, err)),
+    };
+
+    Ok(format!("{} (#{})", resp.title, resp.number))
+  }
+}
+
+
+impl Service for Github {
+  fn unfurl(&self, conf: &config::Config, link: &url::Url) -> Result<String, error::Error> {
+    if let Some(mat) = self.pattern_pr.match_path(link.path()) {
+      self.unfurl_pr(conf, link, mat)
+    }else if let Some(mat) = self.pattern_issue.match_path(link.path()) {
+      self.unfurl_issue(conf, link, mat)
+    }else{
+      Ok(link.to_string())
+    }
   }
 }
 
