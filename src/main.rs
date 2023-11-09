a9e676b2fdb7085a6eb056828b9dd90d5b0637bf
Brian W. Wolter <brianwolter@gmail.com>
2023-11-08 16:21:50 -0800

    ...

--- src/main.rs
+++ src/main.rs
@@ -46,14 +46,6 @@ fn unfurl<R: Read>(opts: &Options, conf: &config::Config, mut r: R) -> Result<()
   r.read_to_string(&mut data)?;
 
   let svc = fetch::Service::instance();
-  // let rx = svc.fetch_urls(vec![
-  //   "https://api.ipify.orz".to_string(),
-  //   "https://api.ipify.org".to_string(),
-  //   "https://api.ipify.org".to_string(),
-  //   "https://api.ipify.org".to_string(),
-  //   "https://api.ipify.org".to_string()
-  // ])?;
-  // println!(">>> >>> >>> {:?}", rx.recv()?);
 
   let mut text: &str = &data;
   let mut toks: Vec<parse::Token> = Vec::new();
@@ -71,7 +63,6 @@ fn unfurl<R: Read>(opts: &Options, conf: &config::Config, mut r: R) -> Result<()
         None => toks.push(parse::Token::Text(text)), // convert to text
       },
     };
-    toks.push(tok.clone());
     text = rest;
   }
 
@@ -94,88 +85,3 @@ fn unfurl<R: Read>(opts: &Options, conf: &config::Config, mut r: R) -> Result<()
   Ok(())
 }
 
-// fn unfurl_request<'a>(conf: &config::Config, data: &'a str, x: usize) -> Result<&'a str, error::Error> {
-//   print!("{}", &data[..x]);
-//   let data: &str = &data[x..];
-//   let (url, rest) = match data.find(char::is_whitespace) {
-//     Some(y) => (&data[..y], &data[y..]),
-//     None    => (data, &data[0..0]),
-//   };
-// 
-//   let url = url::Url::parse(url)?;
-//   let host = match url.host_str() {
-//     Some(host) => host,
-//     None       => {
-//       println!("{}", url);
-//       return Ok(rest); // not a supported URL
-//     },
-//   };
-// 
-//   let svc: Option<Box<dyn service::Service>> = match host.to_lowercase().as_ref() {
-//     service::DOMAIN_GITHUB => Some(Box::new(service::Github::new(conf)?)),
-//     _                      => None,
-//   };
-//   match svc {
-//     Some(svc) => println!("{}", svc.unfurl(conf, &url)?),
-//     None      => println!("{}", url),
-//   }
-// 
-//   Ok(rest)
-// }
-
-// fn next_url<'a>(conf: &config::Config, text: &'a str) -> Result<(&'a str, &'a str, error::Error> {
-//   let (text, rest) = match text.find("https://") {
-//     Some(x) => (&text[..x], &text[x..]),
-//     None    => return (text, ""),
-//   };
-//   let (url, rest) = match rest.find(char::is_whitespace) {
-//     Some(y) => (&text[..y], &text[y..]),
-//     None    => (data, &data[0..0]),
-//   };
-// 
-//   let url = url::Url::parse(url)?;
-//   let host = match url.host_str() {
-//     Some(host) => host,
-//     None       => ,
-//   };
-// 
-//   let svc: Option<Box<dyn service::Service>> = match host.to_lowercase().as_ref() {
-//     service::DOMAIN_GITHUB => Some(Box::new(service::Github::new(conf)?)),
-//     _                      => None,
-//   };
-//   match svc {
-//     Some(svc) => println!("{}", svc.unfurl(conf, &url)?),
-//     None      => println!("{}", url),
-//   }
-// 
-//   Ok(rest)
-// }
-
-// fn unfurl_url<'a>(conf: &config::Config, data: &'a str, x: usize) -> Result<&'a str, error::Error> {
-//   print!("{}", &data[..x]);
-//   let data: &str = &data[x..];
-//   let (url, rest) = match data.find(char::is_whitespace) {
-//     Some(y) => (&data[..y], &data[y..]),
-//     None    => (data, &data[0..0]),
-//   };
-// 
-//   let url = url::Url::parse(url)?;
-//   let host = match url.host_str() {
-//     Some(host) => host,
-//     None       => {
-//       println!("{}", url);
-//       return Ok(rest); // not a supported URL
-//     },
-//   };
-// 
-//   let svc: Option<Box<dyn service::Service>> = match host.to_lowercase().as_ref() {
-//     service::DOMAIN_GITHUB => Some(Box::new(service::Github::new(conf)?)),
-//     _                      => None,
-//   };
-//   match svc {
-//     Some(svc) => println!("{}", svc.unfurl(conf, &url)?),
-//     None      => println!("{}", url),
-//   }
-// 
-//   Ok(rest)
-// }
