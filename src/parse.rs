097684da75a9d4372487206c52410ce2d3471742
Brian W. Wolter <brianwolter@gmail.com>
2023-11-10 17:46:55 -0500

    ...

--- src/parse.rs
+++ src/parse.rs
@@ -28,12 +28,36 @@ pub fn next<'a>(text: &'a str) -> (Token<'a>, &'a str) {
   if x > 0 {
     return (Token::Text(&text[..x]), &text[x..]);
   }
-  match text.find(|c| {
-    char::is_whitespace(c) || c == ',' || c == ';' || c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}'
-  }) {
+  match next_url_end(text) {
     Some(y) => (Token::URL(&text[..y]), &text[y..]),
     None    => (Token::URL(text), ""),
   }
+}
+
+fn is_maybe_url(c: char) -> bool {
+  return c == '.' || c == ':'
+}
+
+fn next_url_end(text: &str) -> Option<usize> {
+  let text = text.chars();
+  let mut p: Option<char> = None;
+  for (i, c) in text.enumerate() {
+    if char::is_whitespace(c) {
+      match p {
+        Some(p) => if is_maybe_url(p) {
+          return Some(i - 1); // move back one
+        } else {
+          return Some(i);
+        },
+        None => return Some(i),
+      }
+    }
+    if c == ',' || c == ';' || c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}' {
+      return Some(i);
+    }
+    p = Some(c);
+  }
+  None
 }
 
 #[cfg(test)]
@@ -42,15 +66,21 @@ mod tests {
   
   #[test]
   fn parse_text() {
-    let text = "Hello, there: https://google.com, and then trailing.";
+    let text = "Hello, there: https://google.com, and then trailing. Also https://yahoo.com.";
     let (tok, text) = next(text);
     assert_eq!(Token::Text("Hello, there: "), tok);
-    assert_eq!("https://google.com, and then trailing.", text);
+    assert_eq!("https://google.com, and then trailing. Also https://yahoo.com.", text);
     let (tok, text) = next(text);
     assert_eq!(Token::URL("https://google.com"), tok);
-    assert_eq!(", and then trailing.", text);
+    assert_eq!(", and then trailing. Also https://yahoo.com.", text);
     let (tok, text) = next(text);
-    assert_eq!(Token::Text(", and then trailing."), tok);
+    assert_eq!(Token::Text(", and then trailing. Also "), tok);
+    assert_eq!("https://yahoo.com.", text);
+    let (tok, text) = next(text);
+    assert_eq!(Token::URL("https://yahoo.com"), tok);
+    assert_eq!(".", text);
+    let (tok, text) = next(text);
+    assert_eq!(Token::Text("."), tok);
     assert_eq!("", text);
     let (tok, text) = next(text);
     assert_eq!(Token::EOF, tok);
